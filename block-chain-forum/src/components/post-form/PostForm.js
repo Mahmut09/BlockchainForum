77df155eeb8eb2167aa440afba75dc0dfde9d439
Mahmut09 <mahmut999@mail.ru>
2023-02-21 23:24:06 +0600

    fix: deleted extra element

---
+++ block-chain-forum/src/components/post-form/PostForm.js
@@ -0,0 +1,11 @@
+function postForm() {
+    return(
+        <form action="#">
+            <input type="text" />
+            <textarea name="post-discription" id="" cols="30" rows="10"/>
+            <button>Запостить</button>
+        </form>
+    )
+}
+
+export default postForm;
--- block-chain-forum/src/index.js
+++ block-chain-forum/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header_tail from './components/header/Header_tail'
 import Header from './components/header/Header'
+import PostForm from './components/post-form/PostForm'
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Header_tail />
-    <Header />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <Header_tail /> */}
+        <Header />
+        <PostForm />
+    </React.StrictMode>
 );
 reportWebVitals();
